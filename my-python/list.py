f1906e733200164636f1dfecd0c81c14bf984c5e
Sushrut <sushrutdeshmukh4@gmail.com>
2025-01-11 11:31:10 +0530

    added files

---
+++ my-python/list.py
@@ -0,0 +1,18 @@
+"""
+list : [1,2,3,4]
+used to store various data of datatypes
+
+ the list contain index number.
+ it can be access by index no. 
+"""
+
+list1 =[3,6,1,4]
+print(list1[0])
+#--------------------------------------------------
+list2 =["poop",6,"lol",2]
+
+list2.append(3)
+print(list2)
+#--------------------------------------------------
+del list2[2]
+print(list2)
