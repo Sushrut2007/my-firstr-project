329ae646f0525f6ed9fe62ae97912b6b7b2acb6b
Sushrut <sushrutdeshmukh4@gmail.com>
2025-01-14 11:16:46 +0530

    Initial commit

---
+++ my-python/area.py
@@ -0,0 +1,8 @@
+# program to calculate area of a rectangle
+
+h = float(input("Enter height of rectangle : "))
+w = float(input("Enter width of rectangle : "))
+
+area = h * w
+
+print(f"Area of rectangle is : {area} ")
