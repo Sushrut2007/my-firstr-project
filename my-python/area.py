0655ba1e34edad6f202e5e403dd8acc458232c4b
Sushrut <sushrutdeshmukh4@gmail.com>
2025-01-25 19:50:52 +0530

    Initial commit

--- my-python/area.py
+++ my-python/area.py
@@ -1,10 +1,9 @@
-# program to calculate area and perimeter  of a rectangle
+# program to calculate area  of a rectangle
 
 h = float(input("Enter height of rectangle : "))
 w = float(input("Enter width of rectangle : "))
 
 area = h * w
-perimeter = 2 * (h + w)
+
 
 print(f"Area of rectangle is : {area} ")
-print(f"Perimeter of rectangle is : {perimeter} ")
